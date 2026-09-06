0522faeb2999ec49f0b5fa3baf1b067288d69155
khanehsefid1-boop <khanehsefid1@gmail.com>
2026-09-07 01:45:19 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,10 +1,12 @@
 export default {
   async fetch(request, env) {
     const response = await env.ASSETS.fetch(request);
+
     const headers = new Headers(response.headers);
     headers.set("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0");
     headers.set("Pragma", "no-cache");
     headers.set("Expires", "0");
+
     return new Response(response.body, {
       status: response.status,
       statusText: response.statusText,
